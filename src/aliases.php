8d53d247dfc978171591aebcb00188babcccc83a
Martin Zlámal <mrtnzlml@gmail.com>
2016-10-14 11:15:26 +0200

    Deprecate this package in favor of adeira/csp

---
+++ src/aliases.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * This file is here only because of BC changes - use Adeira packages instead (https://github.com/adeira).
+ */
+
+class_alias(\Adeira\ContentSecurityPolicyExtension::class, \Mrtnzlml\ContentSecurityPolicyExtension::class);
